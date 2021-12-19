50096db8072642d4cdba47b420132565b6bec162
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-12-19 18:26:38 +0200

    Add exceptions classes

---
+++ src/main/java/lt/codeacademy/exception/ProductNotFoundException.java
@@ -0,0 +1,12 @@
+package lt.codeacademy.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class ProductNotFoundException extends RuntimeException{
+    private UUID uuid;
+}

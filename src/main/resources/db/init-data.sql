bd19eb2d0c88012ca857f882434998ee25252242
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-12-29 16:36:13 +0200

    update init data

---
+++ src/main/resources/db/init-data.sql
@@ -0,0 +1,70 @@
+INSERT INTO PRODUCTS(id, name, category, description, quantity, price)
+VALUES ('48a95af7-8b83-4a08-8001-0f865db8ea26', 'Rolan Snake 60', 'Lankas', 'Lankas Snake yra ištisinis 60 colių lenktasis lankas. Pagamintas iš polimerų kompozito. Tai puikus lankas pradedantiesiems, pramogai bei lankininkų klubams. Univbersalus – tinka ir dešiniarankiams ir kairiarankiams!
+Tempimo jėga: 18, 22, 26 svarai.', 50, 47.99),
+       ('15a95af7-8b83-4a08-8001-0f865db8ea27', 'Core Jet', 'Lankas', 'Puikus lankas pradedantiesiems. Patogi ir lengva metalinė rankena. Versijos: RH ir LH
+Spalvos: Mėlyna, Juoda, Raudona, Rausva, Sidabrinė, Balta, Geltona', 50, 69.99),
+       ('ebdee4f9-5763-4afc-85ed-98b2fdefb35f', 'Ragim Red Deer', 'Lankas', 'Red Deer – puikus elegantiškas ištisinis lenktasis lankas su puikiomis charakteristikomis. Apvalainos formos ir labai patogi rankena. Santykinai trumpas “langas”, tinkamas Field ir 3D varžyboms. Lanko sluoksnių klijavimo dizainas ir medienos spalvų dermė daro šį lanką unikaliu. Skaidraus stiklo pluošto ir anglies pluošto sluoksnių derinys bei aukšštos kokybės mediena garantuoja geras darbines savybes. Dvispalvio fenolio galiukai leidžia naudoti visų tipų temples.
+Ilgis 60
+Tempino jėga nuo 30 iki 50# (kas 5 svarus)
+RH arba LH', 50, 299.00),
+       ('abdee4f9-5763-4afc-85ed-98b2fdefb35d', 'Avalon Tyro', 'Strėlės', 'Avalon pristato naują laisvalaikio strėlę. Puikus sprendimas bet kokiam biudžetui!
+Medžiaga: Anglies pluoštas
+Mažas diametras
+Sparnelių dydis: EP23
+Dydis – ilgis(coliais):
+1500 – 28“
+1300 – 28“
+1100 – 28“
+1000 – 29“
+900 – 29“
+800 – 30“
+700 – 30“
+600 – 31“', 500, 4.00),
+       ('58a95af7-8b83-4a08-8001-0f865db8ea26', 'MP Premium 12 vnt.', 'Strėlės',
+        'Gaminame ir įvairiai dekoruojame medines kedro strėles. Platus spalvų ir stiliaus pasirinkimas pagal jūsų pageidavimą. Parenkame strėles pagal jūsų turimo lanko tempimo jėgą ir lanko tipą. Įvairių spalvų, dydžių ir formos natūralių plunksnu sparneliai. Gaminamos komplektais po 12 vienetų.',
+        10, 120.99),
+       ('45a95af7-8b83-4a08-8001-0f865db8ea27', 'Core Leisure 5mm.', 'Strėles', 'Core pristato naują laisvalaikio stėlę.
+Medžiaga; Stiklo pluoštas
+Tinka lankams iki 25 lbs.
+Ilgis 76 cm.', 220, 1.80),
+       ('abdee4f9-5763-4afc-85ed-98b2fdefb35f', 'Paleidiklis - Tru Ball Boss X', 'Priedai', 'The Boss „X“ yra su plonėjančia rankena ir neturinčiu laisvumų kietai grūdintų nerūdijančio plieno detalių paleidimo mechanizmu.
+
+Jis užtikrina labiausiai stabilų nuleistuko nuspaudimą ir suveikimą šūvio metu ir po jo. Su nauja mažesne galvute kilputė gali būti mažesnė.
+
+Galvutė yra su pagerinta 360 laipsnių veikimo funkcija, eliminuojančia templės perkreipimą.
+
+Boss „X“ yra su mažesne, tylesne templės užkabinimo svirtele bei su atskiru eigos ir jautrumo reguliavimu, su reguliuojama nuleistuko padėtimi. 3 arba 4 pirštų modifikacijos.',
+        10, 139.99),
+       ('cbdee4f9-5763-4afc-85ed-98b2fdefb35d', 'Rankos apsauga Cartel CR-6', 'Priedai', 'Cartel CR-6 – ideali rankos apsauga pradedantiesiems lankininkams, ji tinka plačiai auditorijai – nuo paauglių iki suaugusiųjų. Ši populiari rankos apsauga pagaminta iš stipraus ir lankstaus plastiko. CR 6 yra 165mm ( 6.5″ ) ilgio ir apsaugo rankos dilbį nuo templės smūgio. Ji yra su dviem juodais elastingais reguliuojamo ilgio dirželiais. Tinka tiek dešiniarankiams, tiek kairiarankiams. Su dvejomis eilėmis ventiliacijos skylių, juodos spalvos.
+Pigi, tvirta, bet lanksti apsauga – geras pasirinkimas jaunimui ir studentams.', 10, 5.00),
+       ('58a95af7-8b83-4a08-8001-0f865db8ea23', 'Užmaunama rankų apsauga Avalon Stretchy Guard Slip On', 'Priedai', 'Spalva: juoda, mėlyna, žalia, pilka, oranžinė, violetinė, raudona.
+Dydžiai: S – M – L – XL', 6, 7.90);
+
+INSERT INTO FILES (id, bytes, file_name, media_type, size, timestamp, product_id)
+VALUES ('e5a93d16-2393-47ea-8114-d47b3366f4fa', '42185', 'Lankas-Rolan-Snake.jpg', 'image/jpeg', '40387', null,
+        '48a95af7-8b83-4a08-8001-0f865db8ea26'),
+       ('e2fb4a8d-8f6b-4881-8be6-91d6bcdfdc77', '42184', 'bow-Rolan-Snake.jpg', 'image/jpeg', '44522', null,
+        '48a95af7-8b83-4a08-8001-0f865db8ea26'),
+       ('cd8fee2e-33a2-4160-942b-d276a08cfcee', '42186', 'lankas-pigiau-1.jpg', 'image/jpeg', '33152', null,
+        '48a95af7-8b83-4a08-8001-0f865db8ea26'),
+       ('c17f2e84-267d-4e66-a3f2-aedf49003004', '42288', 'Lankas-Ragim-Red-Deer.jpg', 'image/jpeg', '32917', null,
+        'ebdee4f9-5763-4afc-85ed-98b2fdefb35f') ,
+        ('8dd50bb7-04a8-4eee-adf5-7c522d379352', '42289', 'lankas-core-jet.jpg', 'image/jpeg', '16005', null,
+        '15a95af7-8b83-4a08-8001-0f865db8ea27'),
+       ('3187b480-fce4-4bfd-b515-bc8eb82f9f57', '42291', 'Ragim-Bow-Red-Deer.jpg', 'image/jpeg', '39793', null,
+        'ebdee4f9-5763-4afc-85ed-98b2fdefb35f');
+
+
+INSERT INTO USERS(id, username, name, surname, country, city, street, zip_Code, phone_Number, password)
+VALUES ('b606d6f2-f79c-4219-a33b-68d519b48048', 'user', 'user', 'useriauskas', 'Litvinas', 'Vilnius', 'Savanoriu',
+        '854251', '860532164', '{bcrypt}$2a$10$jYIbAef1H7S.womsk7MRtOCSEx/DgM7CZ1nNeLLzoZ/OPs0a25DV2'),
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', 'admin', 'adminas', 'adminiauskas', 'Litvinas', 'Kaunas', 'Kaunieciu',
+        '854251', '860532164', '{bcrypt}$2a$10$VylYhXDaKC7W28tQTvYYkOdZIj2pnPVIobXOConbXy3xeBcF6Xuni');
+
+INSERT INTO ROLES(id, name)
+VALUES ('777e0319-0f1c-43cf-b1dc-62ce282d7765', 'USER'),
+       ('95be10e4-62d7-4bb9-b70e-92f10cb3f6ab', 'ADMIN');
+INSERT INTO USERS_ROLES(user_id, roles_id)
+VALUES ('b606d6f2-f79c-4219-a33b-68d519b48048', '777e0319-0f1c-43cf-b1dc-62ce282d7765'),
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', '777e0319-0f1c-43cf-b1dc-62ce282d7765'),
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', '95be10e4-62d7-4bb9-b70e-92f10cb3f6ab');

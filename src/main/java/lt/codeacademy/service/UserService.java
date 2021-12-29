adad2d26d55aeb5413939e1b548711335bba2fc3
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-12-29 15:32:05 +0200

    add Create user method

--- src/main/java/lt/codeacademy/service/UserService.java
+++ src/main/java/lt/codeacademy/service/UserService.java
@@ -2,12 +2,21 @@ package lt.codeacademy.service;
 
 
 
+import lt.codeacademy.entity.Role;
+import lt.codeacademy.entity.User;
 import lt.codeacademy.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -20,6 +29,23 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("User %s does not exist", username)));
+    }
+
+    public void createNewUser(User registration) {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        User user = new User(registration.getUsername(),
+                registration.getName(),
+                registration.getSurname(),
+                registration.getCountry(),
+                registration.getCity(),
+                registration.getStreet(),
+                registration.getZipCode(),
+                registration.getPhoneNumber(),
+                encoder.encode(registration.getPassword()),
+                Set.of(new Role(UUID.fromString("777e0319-0f1c-43cf-b1dc-62ce282d7765"), "USER'")));
+        userRepository.save(user);
+    }
+    public void checkUsername(User user){
     }
 
 }
